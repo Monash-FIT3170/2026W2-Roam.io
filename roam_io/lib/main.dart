8a2cc97280e2ec845b65815ba5be9af7af693478
Amar723 <140028404+Amar723@users.noreply.github.com>
2026-04-27 19:37:15 +1000

    ART-3 fixing IOS configuration

--- roam_io/lib/main.dart
+++ roam_io/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:roam_io/features/mapfeature/MapPage.dart';
 
@@ -9,6 +10,10 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await dotenv.load(fileName: '.env');
+
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
